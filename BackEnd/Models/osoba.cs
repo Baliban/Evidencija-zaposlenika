ac9ff009c1b84a284a3f127705fb096b68049327
Antoniomajic <antonio.majich@gmail.com>
2024-03-16 12:44:05 +0100

    nastava subota

---
+++ BackEnd/Models/osoba.cs
@@ -0,0 +1,9 @@
+﻿namespace BackEnd.Models
+{
+    public class osoba : Entitet
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public int? OIB { get; set; }
+    }
+}
